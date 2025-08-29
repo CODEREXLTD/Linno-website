c87e028f8b2a3b689e8f93cfdb8f7ff29b4690bf
kabir-coderex <kabir@coderex.co>
2025-08-29 14:48:45 +0600

    fix the issues - jsx warnings

--- src/components/ui/JoinUsLink.jsx
+++ src/components/ui/JoinUsLink.jsx
@@ -9,8 +9,8 @@ const JoinUsLink = () => {
         </span>
         <span>
             <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 51 52" fill="none">
-            <path d="M47.3784 3.82861L3.10816 48.0989" stroke="#FFC933" stroke-width="6" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M47.3784 36.304V3.82861H14.903" stroke="#FFC933" stroke-width="6" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M47.3784 3.82861L3.10816 48.0989" stroke="#FFC933" strokeWidth="6" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M47.3784 36.304V3.82861H14.903" stroke="#FFC933" strokeWidth="6" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
         </span>
     </Link>
