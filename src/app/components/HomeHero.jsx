628522b8f760002cde8d016ee1535be4210a53fa
abbelal <belal@coderex.co>
2025-10-15 17:48:56 +0600

    hero section done

--- src/app/components/HomeHero.jsx
+++ src/app/components/HomeHero.jsx
@@ -7,57 +7,91 @@ import { memo } from 'react';
 const HomeHero = () => {
     return (
         <>
-            <section className="w-full">
-                <div className="w-full max-w-[1440px] px-2 md:px-4 mx-auto py-12 md:py-16 lg:py-26">
-                    <div className="grid grid-cols-1  lg:grid-cols-2 gap-8 md:gap-10 lg:gap-12 xl:gap-0 w-full">
-
-                        {/* Left Content */}
-                        <div className="flex flex-col gap-[21px] sm:gap-[32px] lg:gap-[42px] justify-start items-start w-full order-2 lg:order-1">
-                            {/* Innovation Badge */}
-                            <div className="flex flex-col gap-[11px] sm:gap-[17px] lg:gap-[22px] justify-start items-start w-full">
-                                <div className="w-full sm:w-4/5 lg:w-[60%]">
-                                    <Badge
-                                        highlight='Innovation.'
-                                        text='Fueled by People'
-                                        className='max-w-[330px] !py-2'
-                                    />
-                                </div>
-                                {/* Main Heading and Description */}
-                                <div className="flex flex-col gap-[15px] sm:gap-[23px] lg:gap-[30px] justify-start items-start w-full">
-                                    <h1 className="text-[30px] md:text-[50px] font-sora leading-tight text-[#0b0b35] w-full">
-                                        <span className="font-normal">Building Tools That Empower </span>
-                                        <span className="title-gradient"> Creators</span>
-                                        <span className="font-normal"> And Businesses Worldwide</span>
-                                    </h1>
-                                    <p className="text-[14px] sm:text-[16px] md:text-[18px] lg:text-[20px] font-inter font-normal leading-[22px] sm:leading-[26px] md:leading-[31px] lg:leading-[35px] text-[#51515f] w-full md:w-[70%]">
-                                        A team with passion for technology and innovation, focused on creating software solutions that solve real-world problems, currently trusted by over 50k users all over the world! 
-                                    </p>
-                                </div>
+            <section className="linno-hero">
+                <div className="linno-container">
+                    <div className="hero-content-wrapper flex flex-col gap-[21px] sm:gap-[32px] lg:gap-[42px] justify-start items-start w-full order-2 lg:order-1">
+                        {/* Innovation Badge */}
+                        <div className="flex flex-col gap-[11px] sm:gap-[17px] lg:gap-[22px] justify-start items-start w-full">
+                            <div className="w-full">
+                                <Badge
+                                    highlight='Innovation.'
+                                    text='Fueled by People'
+                                    className='max-w-[330px] !py-2'
+                                />
                             </div>
-                            {/* CTA Button */}
-                            <Button
-                                variant="primary"
-                                size="lg"
-                                link={true}
-                                href="/products"
-                                label="See What We've Built"
-                                className="text-[14px] sm:text-[16px] md:text-[18px] lg:text-[20px] font-sora font-semibold leading-[18px] sm:leading-[20px] md:leading-[23px] lg:leading-[26px] text-[#ffffff] bg-[#3433fe] rounded-[10px] pt-[7px] sm:pt-[11px] lg:pt-[14px] pr-[16px] sm:pr-[24px] lg:pr-[32px] pb-[7px] sm:pb-[11px] lg:pb-[14px] pl-[16px] sm:pl-[24px] lg:pl-[32px] hover:bg-[#2d2bdb] active:bg-[#2622c4] transition-all duration-200 w-full sm:w-auto"
-                            />
+
+                            {/* Main Heading and Description */}
+                            <div className="flex flex-col gap-[15px] sm:gap-[23px] lg:gap-[30px] justify-start items-start w-full">
+                                <h1 className="linno-hero-title">
+                                    Building Tools That <span className="title-gradient"> Empower Creators</span> And <span className="title-gradient">Businesses</span> Worldwide
+                                </h1>
+
+                                <p className="text-[14px] sm:text-[16px] md:text-[18px] xl:text-[20px] font-inter font-normal leading-[1.6] text-[#51515f] lg:max-w-[500px]">
+                                    A team with passion for technology and innovation, focused on creating software solutions that solve real-world problems, currently trusted by over 50k users all over the world! 
+                                </p>
+                            </div>
                         </div>
 
-                        {/* Right Image */}
-                        <div className="flex justify-center items-center w-full order-1 lg:order-2 mb-6 lg:mb-0">
-                            <div className="w-full px-4 sm:px-0">
+                        {/* CTA Button */}
+                        <Button
+                            variant="primary"
+                            size="lg"
+                            link={true}
+                            href="/products"
+                            label="See What We've Built"
+                            className="text-[14px] sm:text-[16px] md:text-[18px] lg:text-[20px] font-sora font-semibold leading-[18px] sm:leading-[20px] md:leading-[23px] lg:leading-[26px] text-[#ffffff] bg-[#3433fe] rounded-[10px] pt-[7px] sm:pt-[11px] lg:pt-[14px] pr-[16px] sm:pr-[24px] lg:pr-[32px] pb-[7px] sm:pb-[11px] lg:pb-[14px] pl-[16px] sm:pl-[24px] lg:pl-[32px] hover:bg-[#2d2bdb] active:bg-[#2622c4] transition-all duration-200 w-full sm:w-auto"
+                        />
+                    </div>
+                </div>
+
+                <div className="linno-hero-image">
+                    <div className="hero-image-wrapper">
+                        <div className="iamge-side1">
+                            <div className="single-hero-image">
                                 <Image
-                                    src="/images/hero-home.jpg"
-                                    alt="Team collaboration"
-                                    width={676}
-                                    height={576}
-                                    className="w-full h-auto rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300"
+                                    src="/images/home-hero-image1.webp"
+                                    alt="linno hero image 1"
+                                    width={417}
+                                    height={496}
+                                    priority
+                                />
+                            </div>
+
+                            <div className="single-hero-image">
+                                <Image
+                                    src="/images/home-hero-image2.webp"
+                                    alt="linno hero image 2"
+                                    width={417}
+                                    height={220}
                                     priority
                                 />
                             </div>
                         </div>
+
+                        <div className="iamge-side2">
+                            <div className="hero-target">
+                                <Image
+                                    src="/images/target-image.webp"
+                                    alt="target"
+                                    width={124}
+                                    height={124}
+                                    priority
+                                />
+
+                                <span>Innovative Team</span>
+                            </div>
+
+                            <div className="single-hero-image">
+                                <Image
+                                    src="/images/home-hero-image3.webp"
+                                    alt="linno hero image 3"
+                                    width={348}
+                                    height={532}
+                                    priority
+                                />
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </section>
--- src/components/common/Badge.jsx
+++ src/components/common/Badge.jsx
@@ -2,10 +2,10 @@ import { memo } from 'react';
 
 const Badge = ({highlight, text, className = ''}) => {
     return (
-        <div className={`rounded-[10px] pt-[2px] sm:pt-[3px] lg:pt-[4px] pr-[9px] sm:pr-[14px] lg:pr-[18px] pb-[2px] sm:pb-[3px] lg:pb-[4px] pl-[9px] sm:pl-[14px] lg:pl-[18px] ${className}`}
+        <div className={`rounded-[10px] pt-[2px] sm:pt-[3px] lg:pt-[4px] pr-[9px] sm:pr-[14px] lg:pr-[18px] pb-[2px] sm:pb-[3px] lg:pb-[4px] pl-[9px] sm:pl-[14px] lg:pl-[18px] ${className} inline-block` }
             style={{ background: 'linear-gradient(90deg, rgba(52, 51, 254, 0.10) 0%, rgba(0, 200, 83, 0.10) 100%)'}}
         >
-            <span className="text-[15px] sm:text-[18px] lg:text-[20px] font-sora font-normal leading-[20px] sm:leading-[23px] lg:leading-[26px] text-center text-[#000000]"> 
+            <span className="text-[15px] sm:text-[18px] xl:text-[20px] font-sora font-normal leading-[20px] sm:leading-[23px] lg:leading-[26px] text-center text-[#000000]"> 
                 {
                     highlight && <span className='text-[#3433FE]'>{highlight} </span>
                 }
