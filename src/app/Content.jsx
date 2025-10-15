a0a9fa16f7414d9100b41099636f2b4a55c14950
kabir-coderex <kabir@coderex.co>
2025-10-15 10:11:44 +0600

    wip

--- src/app/Content.jsx
+++ src/app/Content.jsx
@@ -1,15 +1,8 @@
 'use client';
 import React, { memo } from 'react';
-import Image from 'next/image';
-
 import Header from '../components/common/Header';
 import Footer from '../components/common/Footer';
-import Button from '../components/ui/Button';
 import TechnologyTabs from '@/components/ui/TechnologyTabs';
-import JoinUsLink from '@/components/ui/JoinUsLink';
-import Badge from '@/components/common/Badge';
-import Link from 'next/link';
-import { products } from './products/ProductsContent';
 import HomeHero from './components/HomeHero';
 import HomeBrands from './components/HomeBrands';
 import Products from './components/Products';
