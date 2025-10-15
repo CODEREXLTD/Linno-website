7882bc146534e0196035f42cadabf5d64edc0b91
kabir-coderex <kabir@coderex.co>
2025-10-15 11:48:06 +0600

    added cta section

--- src/components/ui/JoinUsLink.jsx
+++ src/components/ui/JoinUsLink.jsx
@@ -2,19 +2,16 @@
 import Link from 'next/link';
 
 const JoinUsLink = () => {
-  return (
-    <Link href="/careers" className="flex items-center gap-4">
-        <span className='text-[#FFC933] text-center font-sora font-extrabold underline capitalize text-[36px] leading-tight'>
-            Join Us
-        </span>
-        <span>
-            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 51 52" fill="none">
-            <path d="M47.3784 3.82861L3.10816 48.0989" stroke="#FFC933" strokeWidth="6" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
-            <path d="M47.3784 36.304V3.82861H14.903" stroke="#FFC933" strokeWidth="6" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
-            </svg>
-        </span>
-    </Link>
-  );
+    return (
+        <Link href="/careers" className="flex items-center gap-4">
+            <span className='cta-text-gradient underline'>
+                Join Us
+            </span>
+            <span>
+                <svg className='w-[20px] md:w-[24px] lg:w-[51px] h-[20px] md:h-[24px] lg:h-[51px]' fill="none" width="51" height="51" viewBox="0 0 51 51" xmlns="http://www.w3.org/2000/svg"><path stroke="#21F0A8" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="6" d="M47.378 3.365l-44.27 44.27m44.27-11.795V3.365H14.903" /></svg>
+            </span>
+        </Link>
+    );
 };
 
 export default JoinUsLink;
